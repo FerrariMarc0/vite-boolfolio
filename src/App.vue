47cfaf88842af50f8ae5ecf49e22bf2a865750f5
FerrariMarc0 <marco1010@live.it>
2023-05-25 18:48:12 +0200

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,16 @@
   import AppHeader from './components/AppHeader.vue';
   import AppMain from './components/AppMain.vue';
   import AppFooter from './components/AppFooter.vue';
+  import axios from 'axios';
 
   export default {
     name: 'app',
     data() {
       return {
+        apiBaseUrl: 'http://127.0.0.1:8000/api',
+        apiUrls: {
+          projects: '/projects'
+        },
         projects:[]
       }
     },
@@ -17,19 +22,23 @@
     },
     methods: {
       getProjects() {
-        axios.get('/api/projects').then(response => {
-        this.projects = response.data;
-      });
+
+        axios.get(this.apiBaseUrl + this.apiUrls.projects).then((response) => {
+        this.projects = response.data.results;
+        
+      }).catch((error) => {
+        console.log(error);
+      })
       }
     },
     created(){
-      
+      this.getProjects();
     }
   }
 </script>
 
 <template>
   <AppHeader/>
-  <AppMain/>
+  <AppMain :data="projects"/>
   <AppFooter/>
 </template>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,27 @@
 <script>
     export default {
-        name: 'Main'
+        name: 'Main',
+        props: {
+            data: Object
+        },
     }
 </script>
 
 <template>
-
+    <div class="container">
+        <div class="row my-4 gy-4">
+            <div class="col col-md-3" v-for="project in data">
+                <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.name }}</h5>
+                        <p class="card-text">{{ project.description }}</p>
+                        <a href="#" class="btn btn-primary">Go to</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
