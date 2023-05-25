428da508c76505615e72a099c9e582d85a8f299a
FerrariMarc0 <marco1010@live.it>
2023-05-25 19:57:14 +0200

    don't work

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,11 @@
 <script>
     export default {
         name: 'Main',
+        data(){
+            return {
+                imgUrl: '127.0.0.1:8000/storage/'
+            }
+        },
         props: {
             data: Object
         },
@@ -12,10 +17,10 @@
         <div class="row my-4 gy-4">
             <div class="col col-md-3" v-for="project in data">
                 <div class="card">
-                    <img src="..." class="card-img-top" alt="...">
+                    <img :src="project.image" class="card-img-top" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">{{ project.name }}</h5>
-                        <p class="card-text">{{ project.description }}</p>
+                        <p class="card-text">{{ project.description.substring(0, 150) }}</p>
                         <a href="#" class="btn btn-primary">Go to</a>
                     </div>
                 </div>
